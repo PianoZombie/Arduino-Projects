6ad7eb0921e62621068f8191eda08148d896cc70
PianoZombie <freels2nd@gmail.com>
2026-02-10 22:41:37 -0500

    idk what to title this lowk
    Lowkirkentologiclowstatothenticlauruperkirkiflowilisticexpialibrostaglussinadouarterzipaloozachellamogginrizdeadamentalockinlowingbaunergeticyeetkineticsendergibonoculearnableundereppyeakohotictrance

--- morse_code_project.ino
+++ morse_code_project.ino
@@ -1,21 +1,22 @@
+// The pin connected to the speaker
 #define outpin 8
 
-int debug1 = 3; // Lights up when a dot is registered. When the button is held it'll turn off and switch to debug2, since a dash is registered instead.
-int debug2 = 4; // lights up when a dash is registered
-int input = 5;
-String symbol = "";
+int debug1 = 3; // Lights up when a dot is registered. When the button is held it'll turn off and switch to debug2 since a dash is registered instead.
+int debug2 = 4; // Lights up when a dash is registered.
+int input = 5; // Detects the button being pressed.
+String symbol = ""; 
 bool write = false; // by default the arduino shouldn't print anything because the user hasnt pressed the button yet
 String message = ""; // the cummalative dots and dashes
 
-unsigned long time = 0;
-int buttonPrevState = 0;
+unsigned long time = 0; // Will be used to track how long a button is held for.
+int buttonPrevState = 0; // The current state will constantly be compared to the previous state (this variable) in order to know when to keep track of how long the button has been held.
 bool buttonReleased = true; // by default this is true because the user won't be holding the button when the program starts
 
 void setup() {
     pinMode(input, INPUT);
     pinMode(debug1, OUTPUT);
     pinMode(debug2, OUTPUT);
-    Serial.begin(115200);
+    Serial.begin(115200); // The baud rate is higher than the usual 9600 because the "."s and "-"s won't render properly at 9600 for some reason.
 }
 
 void loop() {
@@ -26,7 +27,9 @@ void loop() {
     A dot is 1 unit
     A dash is 3 units
 
-    The unit of time is an aribtrary value, so for this project one unit = 200ms
+    However, coding a dot to register specifically between 50ms and 150ms would be quite annoying both to me the programmer and the user.
+    Therefore, I'll be using a slightly different system where a dash is one unit of time. If the button is held for anything less than that unit of time a dot will be registered.
+    The unit of time is currently 150ms.
   */
 
   int state = digitalRead(input);
@@ -42,20 +45,21 @@ void loop() {
     buttonPrevState = state;
   }
   if (buttonReleased){
+    noTone(outpin); // stop playing the tone
     digitalWrite(debug1, LOW);
     digitalWrite(debug2, LOW);
 
     // If the button was released right after a hold,
     if (write){
-      message += symbol;
-      Serial.println("\n\n\n\n\n\n\n\n\n\n\n"); // <- clears the screen
-      Serial.println(message); // print the registered symbol
-      write = false; // then prevent the arduino from printing anything again.
+      message += symbol; // add the registered symbol to the message
+     // Serial.println("\n\n\n\n\n\n\n\n\n\n\n"); // <- clears the screen
+      Serial.println(message); // print the new message
+      write = false; // then prevent the arduino from printing anything again
     }
   }
-  // The char to be printed will be determined while the button is being held.
+  // When the button is pressed
   if (!buttonReleased){
-    freqout(1000, 1);
+    tone(outpin, 500); // play a 500 mhz tone
     if (millis() - time >= 150){
       digitalWrite(debug1, HIGH);
       digitalWrite(debug2, LOW);
@@ -68,30 +72,5 @@ void loop() {
     }
     write = true; // Since the button is held, it is guarenteed that a write should be made the next time input is found to be LOW.
   }
-  delay(25); // slows loop down to prevent accidental double clicks from doing anything
-}
-
-void freqout(int freq, int t)  // freq in hz, t in ms
-{
-  int hperiod;                               //calculate 1/2 period in us
-  long cycles, i;
-  pinMode(outpin, OUTPUT);                   // turn on output pin
-
-  hperiod = (500000 / freq) - 7;             // subtract 7 us to make up for digitalWrite overhead
-
-  cycles = ((long)freq * (long)t) / 1000;    // calculate cycles
- // Serial.print(freq);
- // Serial.print((char)9);                   // ascii 9 is tab - you have to coerce it to a char to work
- // Serial.print(hperiod);
- // Serial.print((char)9);
- // Serial.println(cycles);
-
-  for (i=0; i<= cycles; i++){              // play note for t ms
-    digitalWrite(outpin, HIGH);
-    delayMicroseconds(hperiod);
-    digitalWrite(outpin, LOW);
-    delayMicroseconds(hperiod - 1);     // - 1 to make up for digitaWrite overhead
-  }
-pinMode(outpin, INPUT);                // shut off pin to avoid noise from other operations
-
+  delay(25); // slows loop down to prevent the arduino from registering something twice by accident
 }
